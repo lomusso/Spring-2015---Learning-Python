40e82b750ac57f06ead8872644167fdba86975a0
Lauren Musso <lom3@duke.edu>
2015-03-01 13:38:15 -0500

    Searching line by line, while indexing the lines, for the phrase we want, then splitting the line by commas and using the first element from that line.

--- dynadecksearch.py
+++ dynadecksearch.py
@@ -6,24 +6,19 @@ Created on Tue Feb 24 20:35:39 2015
 """
 #!/usr/bin/python3
 
-import re
-
 def main():
-    fh = open('dynadeck.txt')
-    for line in fh:
-        line_number = re.search('CONTROL_TERMINATION', line)
-        line_number = line_number+1
-        list_one = line_number.split(",")
-        model_termination_time = list_one[0]
-        print(model_termination_time)
-        
-        line_number_two = re.search('DATABASE_NODOUT', line)
-        line_number_two = line_number_two+1
-        list_two = line_number_two.split(",")
-        time_step = list_two[0]
-        print(time_step)
-        
-        total_time_steps = (model_termination_time/time_step)+1
-        print(total_time_steps)
+    with open('dynadeck.txt') as fh:
+        c = 0
+        d = 0
+        while c < 1:
+            lines = fh.readlines()
+            if 'CONTROL_TERMINATION' in lines:
+                c = 1
+            else:
+                c = 0
+            d += 1
+        split_line = fh[d+1].split(",")
+        termination_time = split_line[0]
+        print(termination_time)
         
 if __name__ == "__main__": main()
