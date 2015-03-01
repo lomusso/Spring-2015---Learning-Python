f729d0cda138000ba36ab99b483f4d4b845e8656
Lauren Musso <lom3@duke.edu>
2015-03-01 13:52:44 -0500

    using regular expressions to find the words CONTROL_TERMINATION

--- dynadecksearch.py
+++ dynadecksearch.py
@@ -6,19 +6,22 @@ Created on Tue Feb 24 20:35:39 2015
 """
 #!/usr/bin/python3
 
+import re
+
 def main():
-    with open('dynadeck.txt') as fh:
-        c = 0
-        d = 0
-        while c < 1:
-            lines = fh.readlines()
-            if 'CONTROL_TERMINATION' in lines:
+    fh = open('dynadeck.txt')
+    c = 0
+    d = 0
+    while c < 1:
+        for line in fh:
+            line = fh.readlines()
+            if re.search('CONTROL_TERMINATION', line):
                 c = 1
             else:
                 c = 0
             d += 1
-        split_line = fh[d+1].split(",")
-        termination_time = split_line[0]
-        print(termination_time)
+    split_line = fh[d+1].split(",")
+    termination_time = split_line[0]
+    print(termination_time)
         
 if __name__ == "__main__": main()
