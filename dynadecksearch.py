a3fcb289d29839326cb4edce1df650977845ca88
Lauren Musso <lom3@duke.edu>
2015-03-02 21:57:50 -0500

    no message

--- dynadecksearch.py
+++ dynadecksearch.py
@@ -6,21 +6,20 @@ Created on Tue Feb 24 20:35:39 2015
 """
 #!/usr/bin/python3
 
-import re
 
 def main():
     fh = open('dynadeck.txt')
+    lines = fh.readlines()
     c = 0
     d = 0
     while c < 1:
-        for line in fh:
-            line = fh.readlines()
-            if re.search('CONTROL_TERMINATION', line):
+        for lines in fh:
+            if 'CONTROL_TERMINATION' in lines:
                 c = 1
             else:
                 c = 0
             d += 1
-    split_line = fh[d+1].split(",")
+    split_line = lines[d+1].split(",")
     termination_time = split_line[0]
     print(termination_time)
         
