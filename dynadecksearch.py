5759e08443112480152f00f4af1899d4e80b9e75
Lauren Musso <lom3@duke.edu>
2015-03-03 13:41:56 -0500

    FINAL working code with doc strings - now onto units testing

--- dynadecksearch.py
+++ dynadecksearch.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Feb 24 20:35:39 2015
-
+Created on Tue Feb 24 2015
 @author: laurenmusso
 """
 #!/usr/bin/python3
@@ -10,14 +9,14 @@ def main():
     fname = 'dynadeck.txt'
     termination_time = extract_time(fname,'CONTROL_TERMINATION')
     time_step = extract_time(fname,'DATABASE_NODOUT')
-    print(termination_time,time_step)
+    num_timesteps = calculate_num_timesteps(termination_time,time_step)
+    print(termination_time,time_step,num_timesteps)
 
 def extract_time(fname, keyword):
     """
     return the first word on the next line after keyword in fname
     """
     keyword_found = False
-
     with open(fname) as f:
         for line in f:
             if keyword_found is True:
@@ -27,6 +26,13 @@ def extract_time(fname, keyword):
                 keyword_found = True
 
     return t
+    
+def calculate_num_timesteps(termination_time,time_step):
+    """
+    calculate the number of time steps
+    """
+    num_timesteps = float(termination_time)/float(time_step)
+    return num_timesteps
 
 if __name__ == "__main__": main()
 
